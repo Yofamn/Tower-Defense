928da7183c61dca2f354c640363900d9971fa946
Yofamn <167719121+Yofamn@users.noreply.github.com>
2025-03-11 15:32:09 -0500

    CSA 3

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TowerDefense
+{
+    public class Health : MonoBehaviour
+    {
+        public float currentHealth = 100f;
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+
+        void TakeDamage(int damageAmount)
+        {
+
+        }
+
+        public static void TryDamage(GameObject target, int damageAmount)
+        {
+            
+        }
+    }
+}
